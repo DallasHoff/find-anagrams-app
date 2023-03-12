114caa6779476ee72505aca44d0eb9efa82b44d8
Dallas Hoffman <dallashoff@gmail.com>
2023-03-12 01:19:30 -0500

    Add PicoCSS; basic styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.scss';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	main {
+		max-width: 400px;
+		margin: auto;
+		padding: 1.5rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,17 +5,43 @@
 </script>
 
 <form method="POST">
-	<label>
-		Letters
-		<input type="text" name="letters" value={form?.letters ?? ''} />
-	</label>
+	<label for="letters-input">Letters</label>
+	<input
+		id="letters-input"
+		type="text"
+		name="letters"
+		value={form?.letters ?? ''}
+		spellcheck="false"
+		maxlength="16"
+	/>
 	<button type="submit">Find Anagrams</button>
 </form>
 
 {#if form?.anagrams}
-	<ul>
+	<ul class="anagrams-list">
 		{#each form.anagrams as anagram}
 			<li>{anagram}</li>
 		{/each}
 	</ul>
 {/if}
+
+<style lang="scss">
+	label {
+		text-align: center;
+	}
+
+	#letters-input {
+		text-align: center;
+		letter-spacing: 0.5ch;
+	}
+
+	.anagrams-list {
+		max-width: max-content;
+		margin: auto;
+		padding: 0;
+
+		li {
+			list-style-type: none;
+		}
+	}
+</style>
